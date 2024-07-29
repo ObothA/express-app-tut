0cf7104b4f636fe9f9ba3f1e1d136e03f774b76d
Abel Oboth <aoboth@ura.go.ug>
2024-07-29 16:27:14 +0300

    Commit 1.

---
+++ knexfile.js
@@ -0,0 +1,71 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+
+const PGDB_PASSWORD = process.env.PGDB_PASSWORD;
+
+module.exports = {
+
+  // development: {
+  //   client: 'sqlite3',
+  //   connection: {
+  //     filename: './dev.sqlite3'
+  //   }
+  // },
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      host: 'localhost',
+      database: 'express-app',
+      user: 'general',
+      password: PGDB_PASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: `${__dirname}/db/migrations`
+    },
+    seeds: {
+      directory: `${__dirname}/db/seeds`
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
---
+++ routes/users.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+const DB =  require('../services/DB');
+
+/* GET users listing. */
+router.get('/', async function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/users', async function(req, res, next) {
+  const users = await DB('users').select('id', 'name');
+  return res.json(users);
+});
+
+module.exports = router;
